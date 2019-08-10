1bb0df7b8a954284e5934d4affe7eccf7948fb91
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2019-08-10 18:14:51 +0100

    Add make parameters as args not env vars

--- cmd/supply/compile.go
+++ cmd/supply/compile.go
@@ -8,15 +8,14 @@ import (
 type CompileCommand struct {}
 
 func (c *CompileCommand) Run(dir string) error {
-  cmd := exec.Command("make")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = append(
-    os.Environ(),
+  cmd := exec.Command(
+    "make",
     "TARGET=linux-glibc",
     "EXTRA_OBJS=contrib/prometheus-exporter/service-prometheus.o",
     "USE_OPENSSL=1",
   )
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	cmd.Dir = dir
 	return cmd.Run()
 }
